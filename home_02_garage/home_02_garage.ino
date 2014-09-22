4f2d4a14fa136a3f0b1b36d52c34659cdd235c45
Todd Eddy <vr@vrillusions.com>
2014-09-21 22:26:23 -0400

    Cleanup code a bit

--- home_02_garage/home_02_garage.ino
+++ home_02_garage/home_02_garage.ino
@@ -17,21 +17,27 @@
 ISR(WDT_vect) { Sleepy::watchdogEvent(); }
 
 
-//#define DEBUG          // Comment out to disable
-#define JEELINK_ID 1   // Jeelink is id 1 (or 0 to broadcast)
-#define NETGROUP 100   // Netgroup everyone is on
-#define NODE_ID 2      // id of THIS node
+// user settings
+//#define DEBUG        // Comment out to disable
+const int JEELINK_ID = 1;   // Jeelink is id 1 (or 0 to broadcast)
+const int NETGROUP = 100;   // Netgroup everyone is on
+const int NODE_ID = 2;      // id of THIS node
 
-#define DOOR_OPEN 1    // 1 means open
-#define DOOR_CLOSED 0  // 0 means closed
-#define DIO_OFFSET 3   // DIO is port num + 3
 
-//Basic macro for debugging
+// sketch constants
+const int DOOR_OPEN = HIGH;    // Door open when signal is HIGH
+const int DOOR_CLOSED = LOW;   // Door closed when LOW
+const int DIO_OFFSET = 3;      // DIO is port num + 3
+
+
+// Basic macro for debugging
+// Ideally change this to a function once I figure out how to accept anything
 #ifdef DEBUG
   #define DEBUG_PRINT(x) Serial.println(x)
 #else
   #define DEBUG_PRINT(x)
 #endif
+
 
 // TEMP421 code thanks to http://ka1kjz.com (my bookmark no longer points to the source
 // posting though, should still be on there somewhere
@@ -40,35 +46,44 @@ ISR(WDT_vect) { Sleepy::watchdogEvent(); }
 // G - pin1
 // + - pin2
 // A - pin4
-PortI2C temp_port1 (1 /*, PortI2C::KHZ400 */); // Not sure what the commented out part is for
-DeviceI2C temperature1 (temp_port1, 0x2A);  // I2C Address of 0x2A
+
+// Secong parameter that's commented out is the rate of communication
+// Can be KHZMAX (default), KHZ400, or KHZ100
+PortI2C temp_port1 (1 /*, PortI2C::KHZ400 */);
+DeviceI2C temp_dev1 (temp_port1, 0x2A);  // I2C Address of 0x2A
+
 
 // Currently just have sensor connected to port 4
 //int DIO1 = 1 + DIO_OFFSET;
 //int DIO2 = 2 + DIO_OFFSET;
 //int DIO3 = 3 + DIO_OFFSET;
-int DIO4 = 4 + DIO_OFFSET;
+const int DIO4 = 4 + DIO_OFFSET;
 
-int temp_lo = 0;
-int temp_high = 0;
-float tempc = 0;
 
-void get_temperature() {
-  temperature1.send();
-  temperature1.write(0x00); // high byte is on register 0
-  temperature1.receive();
-  temp_high = temperature1.read(1);
-  temperature1.stop();
+// Returns the temperature of tempdev where tempdev is a DeviceI2C object
+int get_temperature(DeviceI2C tempdev) {
+  int temp_lo = 0;
+  int temp_high = 0;
+  float tempc = 0;
+
+  tempdev.send();
+  tempdev.write(0x00); // high byte is on register 0
+  tempdev.receive();
+  temp_high = tempdev.read(1);
+  tempdev.stop();
   
-  temperature1.send();
-  temperature1.write(0x10); // low byte on register 0x10
-  temperature1.receive();
-  temp_lo = temperature1.read(1);
-  temperature1.stop();
+  tempdev.send();
+  tempdev.write(0x10); // low byte on register 0x10
+  tempdev.receive();
+  temp_lo = tempdev.read(1);
+  tempdev.stop();
   
   tempc = (float)temp_lo / 256;
   tempc = tempc + temp_high;
+
+  return tempc;
 }
+
 
 void setup() {
 #ifdef DEBUG
@@ -76,13 +91,14 @@ void setup() {
 #endif
   DEBUG_PRINT("Initializing");
 
-  rf12_initialize(2, RF12_915MHZ, 100);
+  rf12_initialize(NODE_ID, RF12_915MHZ, NETGROUP);
 
-  pinMode(DIO4, INPUT);
-  digitalWrite(DIO4, HIGH);  // turn on pull up
+  pinMode(DIO4, INPUT_PULLUP);
 }
 
+
 void loop() {
+  float tempc = 0;
   int door_status_4 = digitalRead(DIO4);
   word payload[4] = {0,0,0,0};
 
@@ -98,10 +114,8 @@ void loop() {
   }
 #endif
 
-  get_temperature();
-  
+  tempc = get_temperature(temp_dev1);
   DEBUG_PRINT(tempc);
-
   payload[0] = int(tempc * 100);
 
   rf12_sleep(RF12_WAKEUP);
@@ -111,5 +125,3 @@ void loop() {
 
   Sleepy::loseSomeTime(60000);
 }
-
-
